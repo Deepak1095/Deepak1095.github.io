492ddae7709cbd1ee571c874557bf9d9c5956a4d
Deepak1095 <ddhankhar999@gmail.com>
2023-06-20 14:31:31 +0530

    resume

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
     GitHubCalendar(".calendar", "Deepak1095");
 
-    // or enable responsive functionality:
     GitHubCalendar(".calendar", "Deepak1095", { responsive: true });
 
-    // Use a proxy
     GitHubCalendar(".calendar", "Deepak1095", {
        proxy (Deepak1095) {
          return fetch(`https://your-proxy.com/github?user=${Deepak1095}`)
        }
     })
+
+
+    let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
+    let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
+
+    function NewTab() {
+      window.open(
+        "https://drive.google.com/file/d/1arPllevTAd65ylIDQccpJrwS0GY3n6mc/view?usp=share_link",
+        "_blank"
+      );
+    }
