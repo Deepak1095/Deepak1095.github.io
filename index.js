a6ffe5bcb7d3f81004619417f52ea96d4f859cce
Deepak1095 <ddhankhar999@gmail.com>
2023-06-20 12:52:52 +0530

    github calender

---
+++ index.js
@@ -0,0 +1,12 @@
+
+    GitHubCalendar(".calendar", "Deepak1095");
+
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "Deepak1095", { responsive: true });
+
+    // Use a proxy
+    GitHubCalendar(".calendar", "Deepak1095", {
+       proxy (Deepak1095) {
+         return fetch(`https://your-proxy.com/github?user=${Deepak1095}`)
+       }
+    }).then(r => r.text())
