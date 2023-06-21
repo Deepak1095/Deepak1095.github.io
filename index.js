d290d8aec83f5b9d9b4616212b96d423c5e35578
Deepak1095 <ddhankhar999@gmail.com>
2023-06-21 17:25:27 +0530

    github calender and statistics

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+// github calender 
+    GitHubCalendar(".github-calendar", "Deepak1095");
 
-    GitHubCalendar(".calendar", "Deepak1095");
+    GitHubCalendar(".github-calendar", "Deepak1095", { responsive: true });
 
-    GitHubCalendar(".calendar", "Deepak1095", { responsive: true });
-
-    GitHubCalendar(".calendar", "Deepak1095", {
+    GitHubCalendar(".github-calendar", "Deepak1095", {
        proxy (Deepak1095) {
          return fetch(`https://your-proxy.com/github?user=${Deepak1095}`)
        }
     })
 
-
+//resume
     let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
     let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
 
